0dbfc3ef240d216614cd6c79e97db6e6fbea62fb
eusouosamuel <keucndd@gmail.com>
2023-07-07 17:25:50 -0300

    reconfigurando o projeto

---
+++ src/usuarios/Usuarios.jsx
@@ -0,0 +1,38 @@
+const Usuarios = () => {
+    const usuarios = [
+        {
+            codigo: 1,
+            nome: "João"
+        },
+        {
+            codigo: 2,
+            nome: "Samuel"
+        },
+        {
+            codigo: 3,
+            nome: "Mikellainy"
+        },
+        {
+            codigo: 4,
+            nome: "Maria"
+        }
+
+    ];
+
+    return(
+        <>
+            <h1>Lista de Usuarios</h1>
+            <ul>
+            {usuarios.map((item, index) =>
+            <li key={index}>
+                {item.codigo} - {item.nome}
+            </li>
+            )}
+            </ul>
+        </>
+        
+    );
+
+}
+
+export default Usuarios;
